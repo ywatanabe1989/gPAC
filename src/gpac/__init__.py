3337a06b37bcb484e44d5150c139cccf4964d94c
LLEmacs <llemacs@llemacs.com>
2025-04-25 19:25:55 +1000

    Export calculate_pac function

---
+++ src/gpac/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Timestamp: "2025-04-25 17:35:28 (ywatanabe)"
+# File: /ssh:sp:/home/ywatanabe/proj/gPAC/src/gpac/__init__.py
+# ----------------------------------------
+import os
+__FILE__ = (
+    "./src/gpac/__init__.py"
+)
+__DIR__ = os.path.dirname(__FILE__)
+# ----------------------------------------
+
+from ._pac import calculate_pac
+
+__version__ = "0.1.0"
+
+# EOF
